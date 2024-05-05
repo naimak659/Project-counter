c0be839a371306245cfc1f5a8f6f2653c26d04c9
Naim AK <naimak659@gmail.com>
2024-05-05 15:05:04 +0600

    trial-2 added and two counter added

--- main.js
+++ main.js
@@ -49,3 +49,57 @@ reset.addEventListener("click", () => {
   // CounterPositive.classList.replace("display-none", "display");
   countDown();
 });
+
+//
+//
+let reset2 = document.querySelector(".reset2");
+let numberDisplay2 = document.querySelector(".number2");
+let CounterPositive2 = document.querySelector(".CounterPositive2");
+let CounterPositiveStop2 = document.querySelector(".CounterPositiveStop2");
+
+let num2 = 0;
+let intervalId2 = null; // Use a variable to store the interval ID
+
+let countUp2 = () => {
+  if (num2 < 420) {
+    num2++;
+    numberDisplay2.innerHTML = num2;
+  }
+  // else {
+  //   clearInterval(intervalId); // Clear interval when num reaches 420
+  // }
+};
+
+let countDown2 = () => {
+  if (num2 > 0) {
+    num2--;
+    numberDisplay2.innerHTML = num2;
+  } else {
+    clearInterval(intervalId2); // Clear interval when num reaches 0
+  }
+};
+
+// counter ++
+CounterPositive2.addEventListener("click", () => {
+  // countUp();
+  CounterPositive2.classList.replace("display", "display-none");
+  CounterPositiveStop2.classList.replace("display-none", "display");
+  clearInterval(intervalId2); // Clear any existing intervals
+  intervalId2 = setInterval(countUp2, 1); // Assign new interval to intervalId
+});
+
+// counter Stop
+CounterPositiveStop2.addEventListener("click", () => {
+  clearInterval(intervalId2); // Clear the counting interval
+  CounterPositiveStop2.classList.replace("display", "display-none");
+  CounterPositive2.classList.replace("display-none", "display");
+});
+
+// counter --
+reset2.addEventListener("click", () => {
+  clearInterval(intervalId2); // Clear any existing intervals
+  intervalId2 = setInterval(countDown2, 1); // Assign new interval to intervalId
+  CounterPositiveStop2.classList.replace("display", "display-none");
+  CounterPositive2.classList.replace("display-none", "display");
+  // countDown();
+});
