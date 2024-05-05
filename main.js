175cd46e36b184c4495e77cb330401b5399ddb6e
Naim AK <naimak659@gmail.com>
2024-05-05 14:46:38 +0600

    trial-1 & removed log

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let down = document.querySelector(".reset");
+let reset = document.querySelector(".reset");
 let numberDisplay = document.querySelector(".number");
-let up = document.querySelector(".up");
-let upStop = document.querySelector(".upStop");
+let CounterPositive = document.querySelector(".CounterPositive");
+let CounterPositiveStop = document.querySelector(".CounterPositiveStop");
 
 let num = 0;
 let intervalId = null; // Use a variable to store the interval ID
@@ -10,9 +10,7 @@ let countUp = () => {
   if (num < 420) {
     num++;
     numberDisplay.innerHTML = num;
-    console.log("up True");
   } else {
-    console.log("up false");
     clearInterval(intervalId); // Clear interval when num reaches 420
   }
 };
@@ -21,29 +19,30 @@ let countDown = () => {
   if (num > 0) {
     num--;
     numberDisplay.innerHTML = num;
-    console.log("down True");
   } else {
-    console.log("down false");
     clearInterval(intervalId); // Clear interval when num reaches 0
   }
 };
 
-up.addEventListener("click", () => {
-  up.classList.replace("display", "display-none");
-  upStop.classList.replace("display-none", "display");
+// counter ++
+CounterPositive.addEventListener("click", () => {
+  CounterPositive.classList.replace("display", "display-none");
+  CounterPositiveStop.classList.replace("display-none", "display");
   clearInterval(intervalId); // Clear any existing intervals
   intervalId = setInterval(countUp, 1); // Assign new interval to intervalId
 });
 
-upStop.addEventListener("click", () => {
+// counter Stop
+CounterPositiveStop.addEventListener("click", () => {
   clearInterval(intervalId); // Clear the counting interval
-  upStop.classList.replace("display", "display-none");
-  up.classList.replace("display-none", "display");
+  CounterPositiveStop.classList.replace("display", "display-none");
+  CounterPositive.classList.replace("display-none", "display");
 });
 
-down.addEventListener("click", () => {
+// counter --
+reset.addEventListener("click", () => {
   clearInterval(intervalId); // Clear any existing intervals
   intervalId = setInterval(countDown, 1); // Assign new interval to intervalId
-  upStop.classList.replace("display", "display-none");
-  up.classList.replace("display-none", "display");
+  CounterPositiveStop.classList.replace("display", "display-none");
+  CounterPositive.classList.replace("display-none", "display");
 });
